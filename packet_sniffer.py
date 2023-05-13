a514e4c482782884f3633b48bb089ae26e8b13ad
Ahmad <122994877+ahmada1h6@users.noreply.github.com>
2023-05-13 04:08:44 +0300

    Update packet_sniffer.py
    last update for the code 
    http / DNS / FTP / SSH

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -16,3 +16,62 @@ def process_sniffed_packet(packet):
                     break
 
 sniff("eth0")
+
+
+
+#----------------------------------------------------------Sniffing DNS packets:----------------------------------------------------------
+
+#!/usr/bin/env python3
+import scapy.all as scapy
+
+def sniff_dns_packets(interface):
+    scapy.sniff(iface=interface, filter="port 53", prn=process_dns_packet)
+
+def process_dns_packet(packet):
+    if packet.haslayer(scapy.DNS):
+        dns_packet = packet[scapy.DNS]
+        print(dns_packet.summary())
+
+sniff_dns_packets("eth0")
+
+
+#--------------------------------------------------Sniffing FTP packets------------------------------------------------------------------
+
+#!/usr/bin/env python3
+import scapy.all as scapy
+
+def sniff_ftp_packets(interface):
+    scapy.sniff(iface=interface, filter="port 21", prn=process_ftp_packet)
+
+def process_ftp_packet(packet):
+    if packet.haslayer(scapy.TCP) and packet.haslayer(scapy.Raw):
+        ftp_packet = packet[scapy.Raw].load
+        print(ftp_packet)
+
+sniff_ftp_packets("eth0")
+
+#---------------------------------------------------------Sniffing SSH packets-------------------------------------------------
+#!/usr/bin/env python3
+import scapy.all as scapy
+
+def sniff_ssh_packets(interface):
+    scapy.sniff(iface=interface, filter="port 22", prn=process_ssh_packet)
+
+def process_ssh_packet(packet):
+    if packet.haslayer(scapy.TCP) and packet.haslayer(scapy.Raw):
+        ssh_packet = packet[scapy.Raw].load
+        print(ssh_packet)
+
+sniff_ssh_packets("eth0")
+
+
+
+
+
+
+
+
+
+
+
+
